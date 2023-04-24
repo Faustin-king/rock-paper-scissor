0cf024958d737d9918d889ee6512fa1775f9daa5
Faustin <f.nduwumwamiwabami@gmail.com>
2023-04-24 11:03:57 +0200

    Add playSingleRound function in script.js

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+function playSingleRound(prayerSelection, computerSelection) {
+  // Make Prayerselection case-insensitive
+  prayerSelection = prayerSelection.toLowerCase();
+  // Define the winning combinations for Rock Paper Scissors.
+  const winnigCombinations = {
+    rock: 'scissors',
+    paper: 'rock',
+    scissors: 'paper',
+  };
+  // Determine the winner based on winning combinations
+  if (prayerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (winnigCombinations[prayerSelection] === computerSelection) {
+    return `You Win! ${prayerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${prayerSelection}`;
+  }
+}
