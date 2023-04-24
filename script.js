efb43624bd302dac3eaf50bfd18c3412205f7c61
Faustin <f.nduwumwamiwabami@gmail.com>
2023-04-24 11:18:37 +0200

    Add a comment

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function playSingleRound(prayerSelection, computerSelection) {
     return `You Lose! ${computerSelection} beats ${prayerSelection}`;
   }
 }
-
+// Test function by using console.log to see the results.
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
 console.log(playSingleRound(playerSelection, computerSelection));
