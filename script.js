51278c37e4335fae1452624bb98b6156bc91a7d8
Faustin <f.nduwumwamiwabami@gmail.com>
2023-05-04 03:24:41 +0200

    Uncomment the game function

--- script.js
+++ script.js
@@ -39,39 +39,39 @@ scissorsBtn.addEventListener('click', () => {
   playSingleRound('scissors');
 });
 // Test function by using console.log to see the results.
-// const playerSelection = 'rock';
-// const computerSelection = getComputerChoice();
-// console.log(playSingleRound(playerSelection, computerSelection));
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playSingleRound(playerSelection, computerSelection));
 
-// function game() {
-//   let userScore = 0;
-//   let computerScore = 0;
+function game() {
+  let userScore = 0;
+  let computerScore = 0;
 
-//   for (let i = 1; i <= 5; i++) {
-//     const playerSelection = prompt(`Round ${i}: What's your move? (rock/paper/scissors)`).toLowerCase();
-//     const computerSelection = getComputerChoice();
-//     const result = playSingleRound(playerSelection, computerSelection);
+  for (let i = 1; i <= 5; i++) {
+    const playerSelection = prompt(`Round ${i}: What's your move? (rock/paper/scissors)`).toLowerCase();
+    const computerSelection = getComputerChoice();
+    const result = playSingleRound(playerSelection, computerSelection);
 
-//     console.log(`Round ${i}: ${result}`);
+    console.log(`Round ${i}: ${result}`);
 
-//     if (result === 'You win!') {
-//       userScore++;
-//       console.log(`User wins round ${i}. User score: ${userScore}, computer score: ${computerScore}`);
-//     } else if (result === 'You lose!') {
-//       computerScore++;
-//       console.log(`Computer wins round ${i}. User score: ${userScore}, computer score: ${computerScore}`);
-//     } else {
-//       console.log(`Round ${i} is a tie. User score: ${userScore}, computer score: ${computerScore}`);
-//     }
-//   }
+    if (result === 'You win!') {
+      userScore++;
+      console.log(`User wins round ${i}. User score: ${userScore}, computer score: ${computerScore}`);
+    } else if (result === 'You lose!') {
+      computerScore++;
+      console.log(`Computer wins round ${i}. User score: ${userScore}, computer score: ${computerScore}`);
+    } else {
+      console.log(`Round ${i} is a tie. User score: ${userScore}, computer score: ${computerScore}`);
+    }
+  }
 
-//   console.log(`Final score: You ${userScore} - ${computerScore} Computer`);
+  console.log(`Final score: You ${userScore} - ${computerScore} Computer`);
 
-//   if (userScore > computerScore) {
-//     console.log('Congratulations, you win the game!');
-//   } else if (userScore < computerScore) {
-//     console.log('Sorry, you lose the game.');
-//   } else {
-//     console.log('The game is tied!');
-//   }
-// }
+  if (userScore > computerScore) {
+    console.log('Congratulations, you win the game!');
+  } else if (userScore < computerScore) {
+    console.log('Sorry, you lose the game.');
+  } else {
+    console.log('The game is tied!');
+  }
+}
